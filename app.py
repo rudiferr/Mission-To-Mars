2e693c45444ea78be02c74a433b5df48243fedc6
rudiferr <xrt.rudy@gmail.com>
2022-06-29 21:03:16 -0400

    Fixed loading

--- app.py
+++ app.py
@@ -5,20 +5,23 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27018/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+   if mars:
+      return render_template("index.html", mars=mars)
+   else:
+      return redirect("/scrape", code=302)
 
-@app.route("/scraping")
-def scraping():
+@app.route("/scrape")
+def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run(debug=False, use_reloader=False)
+   app.run(debug=False)
